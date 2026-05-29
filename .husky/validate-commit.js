26b5ac4f6c528268e83376c00478df78253a6164
FireDisposal <3295829485@qq.com>
2026-05-29 17:01:52 +0800

    🔧 chore: 整理项目文件，移除过期交付说明

--- .husky/validate-commit.js
+++ .husky/validate-commit.js
@@ -18,15 +18,16 @@ const msgFile = process.argv[2];
 let msg = fs.readFileSync(msgFile, 'utf-8').replace(/\uFEFF/g, '');
 const firstLine = msg.split(/\r?\n/, 1)[0];
 
-const emojis = Object.keys(EMOJI_TYPES).join('');
-const types = Object.values(EMOJI_TYPES).join('|');
-const re = new RegExp(`^([${emojis}]) (${types})(\\(.+\\))?: .+`, 'u');
+const emojiAlt = Object.keys(EMOJI_TYPES).join('|');
+const typesAlt = Object.values(EMOJI_TYPES).join('|');
+const re = new RegExp(`^(${emojiAlt}) (${typesAlt})(\\(.+\\))?: .+`, 'u');
 
 const m = firstLine.match(re);
 if (!m) {
   console.log('');
   console.log('  Format: <emoji> <type>: <description>');
   console.log('');
+  console.log('  Types:');
   for (const [emoji, type] of Object.entries(EMOJI_TYPES)) {
     console.log(`    ${emoji} ${type}`);
   }
@@ -36,8 +37,8 @@ if (!m) {
   process.exit(1);
 }
 
-const emoji = m[1];
-const stripped = firstLine.slice(emoji.length + 1);
+const matchedEmoji = m[1];
+const stripped = firstLine.slice([...matchedEmoji].length + 1);
 const lintFile = msgFile + '.lint';
 fs.writeFileSync(lintFile, stripped + msg.slice(firstLine.length), 'utf-8');
 
