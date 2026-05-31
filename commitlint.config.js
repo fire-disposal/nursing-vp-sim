068b67189418516be0fd78859ac2054feef9bc09
FireDisposal <3295829485@qq.com>
2026-05-31 11:38:24 +0800

    🔧 chore: add merge type to commitlint type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'chore', 'test', 'style', 'ci', 'build', 'perf']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor', 'chore', 'test', 'style', 'ci', 'build', 'perf', 'merge']],
     'type-case': [2, 'always', 'lower-case'],
     'subject-case': [0],
     'subject-empty': [2, 'never'],
