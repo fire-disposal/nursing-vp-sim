a7429173a87d6f8609c6c856355116b7d44e91a8
FireDisposal <3295829485@qq.com>
2026-06-08 14:25:59 +0800

    🐛 fix: QA sidebar button overlap with Layout hamburger on mobile

--- frontend/src/pages/QA.tsx
+++ frontend/src/pages/QA.tsx
@@ -249,7 +249,7 @@ export default function QA() {
         </aside>
 
         <main className="flex-1 flex flex-col min-w-0 bg-background">
-          <Button variant="outline" size="icon-sm" className="absolute top-2 left-2 z-30 md:hidden" onClick={() => setShowSidebar(true)}>
+          <Button variant="outline" size="icon-sm" className="absolute top-2 left-12 z-30 md:hidden" onClick={() => setShowSidebar(true)}>
             <Menu size={16} />
           </Button>
 
